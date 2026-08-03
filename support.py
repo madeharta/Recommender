55fda6cfae5aa76629838c9648c51e4cf36232b4
Made Harta <made.harta@gmail.com>
2026-08-03 14:48:48 +0700

    Update support.py file

--- support.py
+++ support.py
@@ -3,7 +3,14 @@ import sys
 def main():
     print("Processing data...")
     # Return 0 for success, or an integer error code for failure
+    n = 5
+    if (n > 0):
+        print("value of unknown is revealed")
     return 0 
+
+def function_check_student():
+    print("This is just a test function")
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main())
