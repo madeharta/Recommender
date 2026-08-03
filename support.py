4b7d003b9ad0063348c0d5b7fd1be608fbb80a4e
Made Harta <made.harta@gmail.com>
2026-08-03 14:39:10 +0700

    Add support.py file

---
+++ support.py
@@ -0,0 +1,9 @@
+import sys
+
+def main():
+    print("Processing data...")
+    # Return 0 for success, or an integer error code for failure
+    return 0 
+
+if __name__ == "__main__":
+    sys.exit(main())
