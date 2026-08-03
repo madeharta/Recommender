51618c27632e67b965a0157e7152c53a5bfeecef
Made Harta <made.harta@gmail.com>
2026-08-03 15:39:11 +0700

    Commit good to go to branch

--- support.py
+++ support.py
@@ -5,5 +5,9 @@ def main():
     # Return 0 for success, or an integer error code for failure
     return 0 
 
+def good_to_go():
+    print("Here is the good to go function")
+    return 0
+
 if __name__ == "__main__":
     sys.exit(main())
